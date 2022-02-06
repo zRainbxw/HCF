846ff358c46150c9e8410694f205e76d39dbe52b
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2022-02-06 13:10:49 -0300

    Update Loader.php

--- src/hcf/Loader.php
+++ src/hcf/Loader.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace hcf;
+
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase
+{
+
+  public function onEnable(): void
+  {
+  $this->getLogger()->info("Test");
+  }
+
+}
