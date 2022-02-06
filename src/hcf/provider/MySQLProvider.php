c40c1003abd51cb3b58cc6a745a3cfe8ebb10616
vzSpell <98136372+vzSpell@users.noreply.github.com>
2022-02-06 16:11:10 -0300

    MYSQLPROVIDER it was finished

--- src/hcf/provider/MySQLProvider.php
+++ src/hcf/provider/MySQLProvider.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace CBTeam\Ranks\provider;
+
+use CBTeam\Ranks\Cerberus;
+use mysqli;
+
+class MySQLProvider {
+  
+  protected $plugin;
+  
+  protected static $db = null;
+  
+  public function __construct(Cerberus $plugin){
+    $this->plugin = $plugin;
+  }
+  public static function init(): void {
+    $db = new mysqli("IPHOST","NAME","contraseña","s5387_NAME");
+    $db->query("CREATE TABLE IF NOT EXISTS player(playerName TEXT PRIMARY KEY, rank TEXT)");
+    self::$db = $db;
+  }
+  public function get(): mysqli {
+    return self::$db;
+  }
+}
