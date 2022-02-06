19bd140f57f9d4bc28628cafa7cf3872edd8660f
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2022-02-06 02:52:07 -0300

    Update and rename YamlProvider.php to YAMLProvider.php

---
+++ src/hcf/provider/YAMLProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace hcf\provider;
+
+use pocketmine\utils\Config;
+
+use hcf\HCFLoad;
+
+class YamlProvider {
+ 
+/** @here HCFLoad **/ 
+public $load = null;
+
+public function __construct(HCFLoad $main){
+$this->load = $main;
+$this->init();
+}
+
+public function init(): void {}
+
+?>
