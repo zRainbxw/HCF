086ad9fd77d76f601695b766d82c3f709f258156
vzSpell <98136372+vzSpell@users.noreply.github.com>
2022-02-06 12:11:55 -0300

    added base Command RepairCommand

--- src/hcf/commands/RepairCommand.php
+++ src/hcf/commands/RepairCommand.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace hcf\commands;
+
+use hcf\HCF;
+use hcf\PlayHCF;
+
+use pocketmine\item\{Item, Armor, Tool};
+
+use pocketmine\utils\TextFormat as TE;
+use pocketmine\command\{CommandSender, PluginCommand};
+
+class RepairCommand extends PluginCommand {
+	
+	/**
+	 * RepairCommand Constructor.
+	 */
+	public function __construct(){
+		parent::__construct("fix", Loader::getInstance());
+
+		parent::setPermission("fixall.use");
+		parent::setDescription("Can repair the items in your Inventory "); 
+		/** 
+		 * luego Lo sigo si alguien quiere seguirlo no problemaxd 
+		 */
+	}
+}
