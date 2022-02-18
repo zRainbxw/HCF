a4dd04a4d40c5dafed49f76497ddf56b9e7fe2d8
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2022-02-18 03:58:43 -0300

    Update MySQLProvider.php

--- src/hcf/provider/MySQLProvider.php
+++ src/hcf/provider/MySQLProvider.php
@@ -5,21 +5,26 @@ namespace hcf\provider;
 use hcf\Loader;
 use mysqli;
 
-class MySQLProvider {
+class MySQLProvider 
+{
   
-  protected $plugin;
+  protected $database;
+
+  public static function connect(): void 
+  {
+    $loader = Loader::getInstance();
+    $sql = new mysqli($loader->getConfig()->get("mysql")["host"], $loader->getConfig()->get("mysql")["user"], $loader->getConfig()->get("mysql")["password"], $loader->getConfig()->get("mysql")["name"], $loader->getConfig()->get("mysql")["port"]);
+    if (!$sql) {
+      $loader->getLogger()->error("SyntaxError: " . $sql->connect_error);
+      $loader->getLogger()->error("Line Error: " . $sql->connect_errno);
+      return;
+    }
+    //$sql->query("");
+    $this->database = $sql;
+  }
   
-  protected static $db = null;
+  public static function get(): mysqli {
+    return $this->database;
+  }
   
-  public function __construct(Cerberus $plugin){
-    $this->plugin = $plugin;
-  }
-  public static function init(): void {
-    $db = new mysqli("IPHOST","NAME","contraseña","s5387_NAME");
-    $db->query("CREATE TABLE IF NOT EXISTS player(playerName TEXT PRIMARY KEY, rank TEXT)");
-    self::$db = $db;
-  }
-  public function get(): mysqli {
-    return self::$db;
-  }
 }
