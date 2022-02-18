53952aea49d22ccaf49ed5ec20538af98237bf1b
iSrDxv <srclauyt@gmail.com>
2022-02-17 22:23:36 -0300

    Se agrego sistema de version, ayuda a investigar errores de codigo

--- src/hcf/Loader.php
+++ src/hcf/Loader.php
@@ -1,25 +1,41 @@
 <?php
 
-namespace HCF;
-use pocketmine\command\ConsoleCommandSender;
+namespace hcf;
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use libs\invmenu\InvMenuHandler;
  
  class Loader extends PluginBase {
    
-   public const PLUGIN_VERSION = "0.1.9";
+   public const PLUGIN_VERSION = "0.2.3";
    
    public static Loader $instance;
    
+   public function onLoad(): void 
+   {
+     if (self::PLUGIN_VERSION !== $this->getDescription()->getVersion()) {
+      $this->getLogger()->error("Please don't change the PLUGIN_VERSION constant, that helps us investigate plugin errors, thanks");
+      $this->getLogger()->warning("Any changes you make to the plugin version will not be supported by us.");
+     $this->getServer()->getPluginManager()->disablePugin($this);
+     }
+   }
+   
    public function onEnable(): void
    {
-    if (!InvMenuHandler::isRegistered()) {
+     if (!InvMenuHandler::isRegistered()) {
       InvMenuHandler::register($this);
-    }
-    $this->getLogger()->info("=========================================="); 
-    $this->getLogger()->info("==========================================");
-    //$this->getServer()->getNetwork()->setName(str_replace(["&"], ["§"], Loader::getConfiguration("config")->get("server-name")));
+     }
+     $this->getLogger()->info("=========================================="); 
+     $this->saveDefaultConfig();
+     $this->getLogger()->notice("Plugin enabled!!");
+     $this->getLogger()->info("==========================================");
+     $this->getServer()->getNetwork()->setName(str_replace(["&"], ["§"], $this->getConfig()->get("server-name")));
+   }
+   
+   public static function getInstance(): Loader
+   {
+     return self::$instance;
    }
    
 }
