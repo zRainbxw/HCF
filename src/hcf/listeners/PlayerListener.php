319b2960c5bb5ce7972905dd7c07bd86a561f4c8
zBxutii <95422142+zBxutii@users.noreply.github.com>
2022-02-18 03:13:02 -0300

    Join & Quit messages added. PlayerListener.php

--- src/hcf/listeners/PlayerListener.php
+++ src/hcf/listeners/PlayerListener.php
@@ -2,6 +2,7 @@
 
 namespace hcf\listeners;
 
+use pocketmine\utils\TextFormat as TE;
 use pocketmine\event\{
   Listener,
   player\PlayerJoinEvent,
@@ -20,11 +21,15 @@ class PlayerListener extends Listener
   public function joinEvent(PlayerJoinEvent $event): void
   {
     $player = $event->getPlayer();
-  }
+    
+    $event->setJoinMessage(TE::GRAY."[".TE::GREEN."+".TE::GRAY."] ".TE::GREEN.$player->getName().TE::GRAY." entered the server.");
+ }
   
   public function quitEvent(PlayerQuitEvent $event): void
   {
     $player = $event->getPlayer();
+    
+    $event->setQuitMessage(TE::GRAY."[".TE::RED."-".TE::GRAY."]".TE::RED.$player->getName()." left the server.");
   }
   
   public function creation(PlayerCreationEvent $event): void 
